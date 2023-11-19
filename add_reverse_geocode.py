a9f6905f810da98ee31b5ffd2c51fb8023aae583
Auh3b <ngaiyayec@yahoo.com>
2023-11-19 23:44:52 +0200

    added reverse geocoding

---
+++ add_reverse_geocode.py
@@ -0,0 +1,25 @@
+from modules.custom_functions import exportToFile, addReverseGeocodedToDataFrame
+from pandas import read_csv
+import os
+
+
+def main():
+    file_path = input("file path: ")
+    lat = input("lat column: ")
+    lon = input("lon column")
+    output_path = input("output file: ")
+
+    MAPBOX_TOKEN = "MAPBOX_TOKEN"
+
+    token = os.environ.get(MAPBOX_TOKEN)
+
+    df = read_csv(filepath_or_buffer=file_path)
+
+    output_df = addReverseGeocodedToDataFrame(
+        df=df, lon_column=lon, lat_column=lat, token=token)
+
+    exportToFile(output_df, "csv", output_path)
+
+
+if (__name__ == "__main__"):
+    main()

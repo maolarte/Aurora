39fe869ae8d2de9eff0d790755ad5e7750089789
Auh3b <ngaiyayec@yahoo.com>
2024-01-04 14:44:55 +0200

    move function from custom function to geo function

--- modules/custom_geo_functions.py
+++ modules/custom_geo_functions.py
@@ -1,7 +1,109 @@
 import sys
 from pandas import Series
 from geopandas import GeoSeries
+from pandas import DataFrame, Series, concat
+from geopandas import GeoDataFrame
+from copy import deepcopy
+from time import sleep
 from shapely.geometry import Point
+from .custom_functions import changeCountriesByExpression, getProgressIndicator
+
+# Mapbox
+from mapbox import Geocoder
+
+default_missing_value = 999999
+
+
+def getCoordinate(value: str, side: str, valueDict: dict[str, tuple[int, int]], expressionDict: dict[str, str]):
+    try:
+        country = changeCountriesByExpression(value, expressionDict)
+        return valueDict[country][side]
+    except Exception as e:
+        return default_missing_value
+
+
+def processFieldCoordinates(df: DataFrame, columnDict: dict[str, dict[str, str]], valueDict: dict[str, tuple[int, int]], expressionDict: dict[str, str]):
+    local_df = deepcopy(df)
+    for column in columnDict.keys():
+        local_df[columnDict[column]["x"]] = local_df[column].str.lower().apply(
+            lambda x: getCoordinate(x, "x", valueDict, expressionDict))
+        local_df[columnDict[column]["y"]] = local_df[column].str.lower().apply(
+            lambda x: getCoordinate(x, "y", valueDict, expressionDict))
+
+    return local_df
+
+
+def processGeocodeData(data):
+    features = data['features']
+    output = {}
+    for feature in features:
+        _id: str = feature['id']
+        id = _id.split(".")[0]
+        output[f"{id}_text"] = feature["text"]
+        output[f"{id}_place_name"] = feature["place_name"]
+    return output
+
+
+def getMapboxGeocoder(token: str):
+    if (token):
+        return Geocoder(access_token=token)
+    else:
+        raise Exception("Invalid Token")
+
+
+def reverseGeocode(longitude: int, latitude: int, token: str):
+    mb_geocoder = getMapboxGeocoder(token)
+    response = mb_geocoder.reverse(lat=latitude, lon=longitude)
+    if (response.status_code == 200):
+        data = response.json()
+        return data
+    else:
+        return None
+
+
+def processReverseGeoding(data: list[tuple[int, int]], token: str, name: str):
+    _output = []
+    pbar = getProgressIndicator(
+        data=data, desc=f"Processing Reverse Geocoding {name}", size=7, unit="coords")
+    for lon, lat in data:
+        try:
+            result = reverseGeocode(lon, lat, token)
+            _decoded = processGeocodeData(result)
+            _output.append(_decoded)
+            pbar.update(1)
+            sleep(0.1)
+
+        except Exception as e:
+            print(e)
+            _output.append([])
+
+    return _output
+
+
+def addReverseGeocodedToDataFrame(df: DataFrame, lon_column: str, lat_column: str, token: str, name: str):
+    """
+    Takes in a DataFrame with longitude and latitude to produce new fields containing geo-administrative attributes
+
+    """
+    local_df = deepcopy(df)
+    coordinates = list(zip(list(local_df[lon_column].astype(float).to_list()), list(
+        local_df[lat_column].astype(float).to_list())))
+    reversed_geocoded = processReverseGeoding(coordinates, token, name)
+    reversed_geocoded_df = DataFrame(reversed_geocoded)
+    output = concat([local_df, reversed_geocoded_df],
+                    axis=1).fillna(default_missing_value)
+    return output
+
+
+def dataFrameToGeoDataFrame(df: DataFrame, geometry_column_name: str, lat_column: str, long_column: str):
+    """
+    Returns GeoDataFrame from DataFrame with location fields
+    """
+    local_df = deepcopy(df)
+    local_df[geometry_column_name] = Point(
+        local_df[lat_column], local_df[long_column])
+    local_geo_df = GeoDataFrame(local_df)
+    return local_geo_df
 
 
 def getDistance(start: list[int], end: list[int]) -> float:
